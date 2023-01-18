7f85183c746cf9c5d962dcd053d8d1cb2e0e7972
adityapund45 <115693065+adityapund45@users.noreply.github.com>
2023-01-18 17:28:54 +0530

    Add files via upload

---
+++ Controller/Service.js
@@ -0,0 +1,21 @@
+const service = require("../Model/Service");
+
+exports.createService = async (req, res) => {
+  const data = await service.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: data,
+    message: "service added successfully",
+  });
+};
+
+exports.getService = async (req, res) => {
+  console.log("hello i called");
+  const data = await service.find();
+  console.log(service);
+  res.status(200).json({
+    success: true,
+    data: data,
+    message: "data fetch successfully",
+  });
+};
